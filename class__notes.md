de0d5fab0b576d7c0b9c89f3e762749e14ae0e87
Jordy <jordycorporan95@gmail.com>
2023-06-21 11:05:58 -0400

    Added notes and picture for references

---
+++ class__notes.md
@@ -0,0 +1,30 @@
+# Callbacks
+
+A callback is just when a function calls another function.
+
+
+## Context
+
+Context in Js referes to what his is in a function
+varies depending on how a function is invoked
+
+
+```js
+Object.func()
+
+```
+Every function is an object.
+
+but not every object is a function !!!!
+
+
+![Different_Invoketions](/Invoke_Ways.png)
+
+
+Ways to call a function
+
+1.Funtion style
+2.method style
+3.Constructor Style
+
+theres no return in a contructor style function
---
+++ lecture_code.js
@@ -0,0 +1,54 @@
+function calculator(operationCallback,num1,num2){
+    return operationCallback(num1,num2);
+}
+
+
+//? ES5
+
+function SomeClass() {
+
+}
+SomeClass.prototype,someFunction = function () {
+
+}
+
+
+//? ES6
+
+class SomeClass {
+    constructor () {
+        // contructor functionality
+    }
+
+    someFunction () {
+        // 'Instance method' functionality
+    }
+}
+
+
+//!Mostly ES5 for the test
+
+
+
+// function Cat(name,owner) {
+//     this.name = name
+//     this.owner = owner
+// }
+
+
+
+
+// class Cat {
+//     constructor(name, owner) {
+//         this.name = name;
+//         this.owner = owner;
+//     }
+//     cuteStatement() {
+//         return `${this.owner} loves ${this.name}.`
+//     }
+
+
+// }
+
+
+
