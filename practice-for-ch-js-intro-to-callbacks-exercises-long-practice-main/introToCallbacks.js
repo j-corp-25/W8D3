ee8df07edd1221451bdce5839e44fbd5cbb0a34f
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-06-21 14:44:02 -0400

    Completed Clock

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/introToCallbacks.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/introToCallbacks.js
@@ -4,15 +4,42 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function Clock(hour,minute){
-    this.hour
-    this.minute
+class Clock {
+    constructor(){
+        this.date = new Date();
+        this.hh = this.date.getHours();
+        this.mm = this.date.getMinutes();
+        this.ss = this.date.getSeconds();
+        this._tick()
+    }
+
+
+    printTime(){
+        let time = `${this.hh}:${this.mm}:${this.ss}`
+        console.log(time)
+        this.ss ++ 
+
+        if(this.ss === 60){
+            this.mm ++
+            this.ss = 0
+        }
+
+        if(this.mm === 60){
+            this.hh ++
+            this.mm = 0
+        }
+
+        if(this.hh === 24){
+            this.hh = 0
+            this.ss = 0
+            this.mm = 0
+        }        
+    }
+
+    _tick(){
+        setInterval(this.printTime.bind(this),1000)   
+    }
 }
 
-Clock.prototype._tick = function(){
-    
-}
-
-Clock.prototype.printTime = function(){
-
-}
+const clock = new Clock();
+ 
